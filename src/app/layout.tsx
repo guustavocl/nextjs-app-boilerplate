9052965e8b26e48ab0c2466e9c6b998a7de941eb
guustavocl <gustavo@gus.sh>
2023-09-20 23:32:13 -0300

    toast and loader

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import Header from "@/components/Header";
 import { Nunito_Sans } from "next/font/google";
+import ToastProvider from "@/providers/ToastProvider";
+import NextTopLoader from "nextjs-toploader";
 import "./globals.css";
+import "react-toastify/dist/ReactToastify.css";
+import "tippy.js/animations/perspective.css";
+import "tippy.js/dist/tippy.css";
+import "tippy.js/themes/translucent.css";
 
 const nunito = Nunito_Sans({
   weight: "400",
@@ -14,16 +20,14 @@ export const metadata = {
   description: "Created by @guustavocl [https://gus.sh]",
 };
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={nunito.className}>
         <Header />
+        <NextTopLoader color="#6d28d9" showSpinner={false} />
         {children}
+        <ToastProvider />
       </body>
     </html>
   );
